ab646aeb7e6c15470f35149f6b97a8946c0141ad
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-10-01 13:36:35 -0700

    firstcommit

---
+++ app.js
@@ -0,0 +1,239 @@
+
+//ch=1
+
+// alert ("please enter your value")
+
+//ch=2
+
+// var names='faizahmed'
+// console.log(names)
+
+
+//ch=3
+
+// var num1=3
+// var num2=6
+// console.log(num1+num2)
+
+//ch=5
+
+
+// var num1=6
+// var num2=9
+
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1%num2)
+
+
+//ch=6
+
+
+// var num = 1
+//  var newNum = num++
+// console.log(newNum)
+
+
+//ch=7
+
+// var price1=50
+// var price2=100
+// var totalprice=150
+
+// console.log(price1+price2*totalprice)
+
+
+//ch=8
+ 
+
+// const name='faiz ahmed'
+
+// console.log(  " my name is "+    name )
+
+
+
+//ch=9
+// const names=prompt('enter your name')
+
+// console.log(names)
+
+
+//ch=10
+
+// const num = prompt('enter your number')
+// if(num==='15'){
+
+// console.log('correct')
+
+// }
+
+
+//CH=11
+
+
+// const num=prompt('enter your number')
+
+// if(num>='50'){
+
+// console.log('correct')
+
+
+// }
+
+
+//ch=12
+
+
+// const num = prompt('enter your number')
+
+// if(num==='15'){
+
+// console.log('correct')
+
+// }
+
+
+// else(
+
+// console.log('incorrect')
+
+
+// )
+
+
+//ch=13
+
+// const name= prompt('enter your name')
+
+// if(name<='faizwaseem' ){
+//         console.log('correct')
+    
+// }
+
+//ch=14
+
+
+// const num=prompt ('enter num')
+
+// if(num==='50'){
+
+
+//     if( num==='100'  ){
+
+//         console.log('correct')
+// }
+// console.log()
+
+// }
+
+
+
+//ch=15
+
+// const arr=['manog','banana','apple']
+// console.log(arr)
+
+
+//ch=16
+
+    
+// const arr=['mango','banana','apple']
+// console.log(arr)
+
+// arr.push('peach')
+// console.log(arr)
+
+// arr.pop('peach')
+
+// console.log(arr)
+
+
+
+
+//ch=16
+
+
+// const arr=['mango','banana','apple']
+// console.log(arr)
+
+// arr.unshift('peach')
+// console.log(arr)
+
+// arr.shift('peach')
+// console.log(arr)
+
+// arr.splice(0,1,'peach' )
+// console.log(arr)
+
+// const arrslice=arr.slice(0,2 )
+
+// console.log(arrslice)
+
+
+
+//ch=20
+
+// var firstNames = [ "Lojack ", "Gizmo ", "Do-Rag "];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+//  for (var i = 0; i < firstNames.length; i++) {
+//  for (var j = 0; j < lastNames.length; j++) {
+//  fullNames.push(firstNames[i] + lastNames[j]);
+// }
+// console.log(fullNames)
+// }
+
+
+//CH=21
+
+// var firstNames = [ "Lojack ", "Gizmo ", "Do-Rag "];
+// console.log(firstNames)
+
+// // var names= firstNames.touppercase()
+// // console.log(names)
+
+
+
+
+//ch=27
+
+// const randomnumber=Math.random()
+// console.log(randomnumber)
+
+
+//ch=29
+//wrong
+
+// const num= 124556 
+// const nums=num.toString()
+
+// console.log(nums)
+
+
+
+//ch=31,32
+
+
+// var rightNow = new Date();
+// var theDay = rightNow.getDay();
+
+// console.log(theDay)
+
+// var theDay = rightNow.getTime();
+
+// console.log(theDay)
+
+
+
+//ch=33
+
+// var rightNow = new Date('september 29,2023');
+// console.log(rightNow)
+
+
+
+
+
+
+
